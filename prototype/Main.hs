cb62080d02036ba288f5b75556acadd066644d43
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2014-09-11 17:48:41 +0200

    added lg1657873

--- prototype/Main.hs
+++ prototype/Main.hs
@@ -27,6 +27,19 @@ c = tail . concat
   . map (map (HexPoint.id_of_point 5 . HexPoint.mirror 2 . Point2D.hexangular 5))
   . points_from_lg
 
+breakL :: (a -> Bool) -> [a] -> [[a]]
+breakL sep xs = case break sep xs of (l,[]) -> [l]
+                                     (l,_:rs) -> l : breakL sep rs
+
+splitAtL :: Int -> [a] -> [[a]]
+splitAtL m xs = case splitAt m xs of (l,[]) -> [l]
+                                     (l,rs) -> l : splitAtL m rs
+
+lg_from_string :: String -> [[String]]
+lg_from_string = map (splitAtL 2 . tail . dropWhile (/='.')) . breakL (==' ')
+
+lg1657873 :: String
+lg1657873 = "1.G4 2.F5E7 3.E5D7 4.D6E4 5.F3D4 6.C5G3 7.D5E6 8.F6F4E3 9.D8E8F2 10.I2D2 11.B6G7 12.C6H5 13.B7E2 14.D3C4 15.G5G6F7 16.C8I3 17.H3H2 18.G2H1C7 19.B8B9C9 20.I1B5A6 21.A7H4I4 22.E1F1 23.G1D9I5 24.B4C3 25.H6F8E9 26.A5A8A9"
 
 lg1657875 :: [[String]]
 lg1657875 = [ ["E5"]
